ed86439d80c2dc854c97e66df94c4232d0684949
Jason Stajich <jasonstajich.phd@gmail.com>
2018-12-12 16:40:34 -0800

    build recipe updated

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/src
-mkdir -p $PREFIX/utils
-mkdir -p $PREFIX/lib
-mkdir -p $PREFIX/data
-mkdir -p $PREFIX/sample
+FGMP_HOME=$PREFIX/opt/fgmp-${PKG_VERSION}
 
-cp $SRC_DIR/src/* $PREFIX/src
-cp -R $SRC_DIR/utils/* $PREFIX/utils
-cp $SRC_DIR/README.md $PREFIX
-cp $SRC_DIR/data/* $PREFIX/data
-cp $SRC_DIR/fgmp.config $PREFIX
-cp $SRC_DIR/lib/FGMP.pm $PREFIX/lib
-cp $SRC_DIR/sample/* $PREFIX/sample
-cp $SRC_DIR/sample_output/* $PREFIX/sample_output
+# why not use rsync?
+cp -R $SRC_DIR/src ${FGMP_HOME}
+cp -R $SRC_DIR/utils ${FGMP_HOME}
+cp $SRC_DIR/README.md ${FGMP_HOME}
+cp -R $SRC_DIR/data ${FGMP_HOME}
+cp $SRC_DIR/fgmp.config ${FGMP_HOME}
+cp -R $SRC_DIR/lib ${FGMP_HOME}
+cp -R $SRC_DIR/sample ${FGMP_HOME}
+cp -R $SRC_DIR/sample_output ${FGMP_HOME}
 
-chmod 755 $PREFIX/src/*
-chmod 755 $PREFIX/utils/*
-chmod 755 $PREFIX/lib/*
-chmod 755 $PREFIX/bin
+chmod 755 -R ${FGMP_HOME}/src ${FGMP_HOME}/utils ${FGMP_HOME}/lib ${FGMP_HOME}/bin
 
-ln -s $PREFIX/src/fgmp.pl $PREFIX/fgmp
-ln -s $PREFIX/lib/FGMP.pm $PREFIX/FGMP.pm
+ln -s ${FGMP_HOME}/src/fgmp.pl ${FGMP_HOME}/fgmp
